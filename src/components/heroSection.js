36f25f42496e43c35b24ce7262cd8d4cf0d06f33
Rishika <rishikarocky14@gmail.com>
2021-10-02 02:02:29 +0530

    Temporary push to host

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -6,10 +6,10 @@ function HeroSection() {
     <div id="herosection" className="hero">
       <div className="container-fluid">
         <div className="row foss-heading">
-          <div className="col-12 col-md-4 col-lg-4 d-none d-md-block">
+          <div className="col-12 col-md-4 col-lg-3 offset-0 offset-lg-1 d-none d-md-block">
             <img src="assets/images/logo.png" alt="Foss Cell GECT logo" />
           </div>
-          <div className="col-10 col-md-8 col-lg-8">
+          <div className="col-10 col-md-8 col-lg-8 offset-1 offset-md-0">
             <h1>FOSS CELL</h1>
             <h1>GEC THRISSUR</h1>
           </div>
